c7bb1104f7b5171f1a66662f9719063fc3146e19
123chenna <keshavachenna330@gmail.com>
2026-04-17 16:17:03 +0530

    Update Orders.java

--- src/main/java/com/ecommerce/model/Orders.java
+++ src/main/java/com/ecommerce/model/Orders.java
@@ -27,7 +27,7 @@ public class Orders {
     
     private String status;
 
-   @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+  @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssXXX")
 private ZonedDateTime orderTime;
 
 @PrePersist
